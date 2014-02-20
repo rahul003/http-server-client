60e1896f6938691001eeaeccae1f4989b2e18f47
Rahul Huilgol <rahulhuilgol@gmail.com>
2014-02-20 21:33:43 +0530

    clean

--- fb_files/YT3KHj2muA-.js
+++
@@ -1,5 +0,0 @@
-/*!CK:2940971875!*//*1384887159,173199715*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["8eJ2b"]); }
-
-__d("legacy:PhotoSnowliftAds",["PhotoSnowliftAds"],function(a,b,c,d){a.PhotoSnowliftAds=b('PhotoSnowliftAds');},3);
--- testfiles/fb_files/XZ2-FjjwKeI.js
+++
@@ -1,6 +0,0 @@
-/*!CK:2733291600!*//*1386434340,173217053*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["Zkght"]); }
-
-__d("legacy:photos-constants",["PhotosConst"],function(a,b,c,d){a.PhotosConst=b('PhotosConst');},3);
-__d("legacy:PhotoTagger",["PhotoTagger"],function(a,b,c,d){a.PhotoTagger=b('PhotoTagger');},3);
